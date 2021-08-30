0a65bb2e648ba7384d0eb39db0366b1be3b3ec37
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-08-30 15:55:46 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_123642) do
+ActiveRecord::Schema.define(version: 2021_08_30_125346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_08_30_123642) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image"
+    t.integer "price"
+    t.integer "stock_number"
+    t.string "sku"
+    t.bigint "region_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["region_id"], name: "index_products_on_region_id"
+  end
+
   create_table "regions", force: :cascade do |t|
     t.string "title"
     t.text "country_details"
@@ -40,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_08_30_123642) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "products", "regions"
 end
