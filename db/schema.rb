75f2f0fb3802d150e661915ad8832911f2b9faf6
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-08-30 15:35:00 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_115630) do
+ActiveRecord::Schema.define(version: 2021_08_30_122811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "email"
