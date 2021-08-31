c72a575a273f1aa904dec9a42905dc29f7741db7
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-08-31 12:34:47 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_092435) do
+ActiveRecord::Schema.define(version: 2021_08_31_093219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_092435) do
     t.bigint "customer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "total_amount"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
