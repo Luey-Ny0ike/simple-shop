31779abea44c37cf67c69915db69dc4d33855da1
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-08-31 14:09:21 +0300

    Custom method to create order items

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
   # Associations
   belongs_to :customer
   has_many :order_items
+
+  # custom methods
+  def create_order_items
+    # We will eventually process this using params from the associated cart
+    # For now I just choose 2 products at random
+    products = Product.limit(10).sample(2)
+    products.each do |p|
+      OrderItem.create(order_id: self.id, product_id: p.id)
+    end
+  end
 end
