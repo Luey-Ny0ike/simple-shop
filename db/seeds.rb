5787810a24c9cd29f981fcc64c28446593d10d3d
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-09-01 17:36:39 +0300

    Added seed data, will probably come add faker for richer data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do |i|
+  Region.create(title: "Region #{ i+1 }", country_details: "country details here", tax_details: "tax details here", currency_details: "currency_details here" )
+end
+
+10.times do |i|
+  Product.create(
+    title: "product #{ i+1}",
+    description: "description here",
+    price: rand(200..2000),
+    sku: rand(36**7).to_s(36),
+    stock_number: rand(0..55),
+    region: Region.all.sample
+  )
+end
