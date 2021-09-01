3e6842979576f3d88a94bf778f2cf2d45aa7a3f6
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-09-01 12:57:58 +0300

    defined the devise routes and scoped them under api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  scope :api, defaults: { format: :json } do
+    devise_for :users, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
     get 'current_user', to: 'current_user#index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json } do
     namespace :v1 do
