2d1f0ff900e0f90cb9fae54e11bcc1c8555d031e
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-09-06 13:18:44 +0300

    Updated the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |i|
+  User.create(
+    email: "test#{ i+1 }@test.com",
+    password: 1234567,
+    password_confirmation: 1234567,
+    confirmed_at: DateTime.now
+  )
+end
+
 4.times do |i|
   Region.create(title: "Region #{ i+1 }", country_details: "country details here", tax_details: "tax details here", currency_details: "currency_details here" )
 end
@@ -18,5 +27,13 @@ end
     sku: rand(36**7).to_s(36),
     stock_number: rand(0..55),
     region: Region.all.sample
+  )
+end
+
+5.times do |i|
+  Order.create(
+    full_name: "Test user #{ i+1 }",
+    email: "test#{ i+1 }@test.com",
+    customer: Customer.all.sample
   )
 end
