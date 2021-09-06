9c8f393982b4d7e5aa56a3c155d4f5f999001d9c
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-09-06 20:47:29 +0300

    Moved the custom devise controllers to the namespaced folders

---
+++ app/controllers/api/v1/current_user_controller.rb
@@ -0,0 +1,8 @@
+module Api::V1
+  class CurrentUserController < ApplicationController
+    before_action :authenticate_user!
+    def index
+      render json: current_user, status: :ok
+    end
+  end
+end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
+class Api::V1::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   respond_to :json
 
@@ -45,6 +45,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   # end
 end
