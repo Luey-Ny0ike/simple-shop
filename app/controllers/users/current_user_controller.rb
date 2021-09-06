a54baba90e056dd39670129d31538711137996b7
Luey-Ny0ike <nyoikelewis@gmail.com>
2021-09-06 13:26:09 +0300

    Structured the current user controller better

--- app/controllers/users/current_user_controller.rb
+++ app/controllers/users/current_user_controller.rb
@@ -1,4 +1,4 @@
-class CurrentUserController < ApplicationController
+class Users::CurrentUserController < ApplicationController
   before_action :authenticate_user!
   def index
     render json: current_user, status: :ok
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
     }
-    get 'current_user', to: 'current_user#index'
+    get 'users/current_user', to: 'users/current_user#index'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json } do
